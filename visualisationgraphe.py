cdfd15f6a8c736216fd32fba7d4efddf980eddfc
naitslimane <126019036+naitslimane@users.noreply.github.com>
2023-03-12 17:29:29 +0100

    Add files via upload

---
+++ visualisationgraphe.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import warnings
+import itertools
+import numpy as np
+import matplotlib.pyplot as plt
+warnings.filterwarnings("ignore")
+plt.style.use('fivethirtyeight')
+import pandas as pd
+import statsmodels.api as sm
+import matplotlib
+from pandas import datetime
+
+from pylab import rcParams
+
+
+# Load the dataset from a CSV
+def parser(x):
+    return datetime.strptime(x,'%m%Y')
+data = pd.read_csv('ts_brutt.csv', sep=";",index_col=1,parse_dates=[1], squeeze=True, date_parser=parser)
+data['valeur']=data.valeur.fillna(0)
+
+# afficher le graphe de chaque client
+for client in data.client.unique():
+    client_data = data[data.client == client]
+    st.subheader(f"Client {client}")
+    st.line_chart(client_data['valeur'])
